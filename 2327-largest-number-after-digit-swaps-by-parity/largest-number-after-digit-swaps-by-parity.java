63c5fe25bde6fb33bf35a676eb084dc1e6745d72
Ganesh0225 <tmssrinivasarao@gmail.com>
2026-09-01 21:37:17 +0530

    Time: 1 ms (98.77%) | Memory: 42.3 MB (76.45%) - LeetSync

--- 2327-largest-number-after-digit-swaps-by-parity/largest-number-after-digit-swaps-by-parity.java
+++ 2327-largest-number-after-digit-swaps-by-parity/largest-number-after-digit-swaps-by-parity.java
@@ -2,17 +2,16 @@ class Solution {
     public int largestInteger(int num) {
         PriorityQueue<Integer> odd=new PriorityQueue<>(Collections.reverseOrder());
         PriorityQueue<Integer> even=new PriorityQueue<>(Collections.reverseOrder());
-        int m=num;
-        while(m>0){
-            int rem=m%10;
-            if(rem%2==0) even.offer(rem);
-            else odd.offer(rem);
-            m/=10;
+        char[] digits=String.valueOf(num).toCharArray();
+        for(char ch:digits){
+            int d=ch-'0';
+            if(d%2==0) even.offer(d);
+            else odd.offer(d);
         }
-        String s=String.valueOf(num);
         int ans=0;
-        for(char ch:s.toCharArray()){
-            if((ch-'0')%2==0) ans=ans*10+even.poll();
+        for(char ch:digits){
+            int d=ch-'0';
+            if(d%2==0) ans=ans*10+even.poll();
             else ans=ans*10+odd.poll();
         }
         return ans;
