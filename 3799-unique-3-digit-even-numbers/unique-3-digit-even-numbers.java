3547a6df31517b3a287372744c1447e080dac27d
Ganesh0225 <tmssrinivasarao@gmail.com>
2026-09-11 21:22:10 +0530

    Time: 8 ms (32.13%) | Memory: 43.6 MB (98.80%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -4,7 +4,11 @@ class Solution {
         int res = 0;
 
         for (int d : digits) f[d]++;
-
+ 
+        // Iterate through all possible 3-digit even numbers:
+        // i: hundreds place (1-9, no leading zero)
+        // j: tens place (0-9)
+        // k: units place (0, 2, 4, 6, 8 for even)
         for (int i = 1; i < 10; i++) 
             for (int j = 0; j < 10; j++) 
                 for (int k = 0; k < 9; k += 2) {
